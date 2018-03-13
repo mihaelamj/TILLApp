745751a4db564cabdd6956653b0ede225e289a28
Mihaela MJ <mihaelamj@me.com>
2018-03-13 03:17:16 +0100

    passing data to the index.leaf page

--- Sources/App/Controllers/WebsiteController.swift
+++ Sources/App/Controllers/WebsiteController.swift
@@ -6,8 +6,13 @@ struct WebsiteController: RouteCollection {
     router.get(use: indexHandler)
   }
   
+//  func indexHandler(_ req: Request) throws -> Future<View> {
+//    return try req.leaf().render("index")
+//  }
+  
   func indexHandler(_ req: Request) throws -> Future<View> {
-    return try req.leaf().render("index")
+    let context = IndexContent(title: "Homepage")
+    return try req.leaf().render("index", context)
   }
 }
 
@@ -15,4 +20,9 @@ extension Request {
   func leaf() throws -> LeafRenderer {
     return try self.make(LeafRenderer.self)
   }
+}
+
+//struct to hold the data for a page
+struct IndexContent: Encodable {
+  let title: String
 }
